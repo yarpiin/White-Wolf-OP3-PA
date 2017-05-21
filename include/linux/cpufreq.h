cd897c950ba41919e8d177809284221dd952a600
DespairFactor <matthewalex@linux.com>
2017-05-21 14:37:58 +0100

    cpufreq: introduce despair governor

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -516,6 +516,9 @@ extern struct cpufreq_governor cpufreq_gov_darkness;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_ALUCARD)
 extern struct cpufreq_governor cpufreq_gov_alucard;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_alucard)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_DESPAIR)
+extern struct cpufreq_governor cpufreq_gov_despair;
+#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_despair)
 #endif
 
 /*********************************************************************
