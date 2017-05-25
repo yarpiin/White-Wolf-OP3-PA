f2cf2a979c5e6118ff320db0b8b7cb9f9976ebf0
Alucard24 <no@email.org>
2017-05-25 20:35:32 +0100

    DYNAMIC_FSYNC: added support for STATE_NOTIFIER.

--- drivers/video/msm/mdss/mdss_dsi.c
+++ drivers/video/msm/mdss/mdss_dsi.c
@@ -23,7 +23,9 @@
 #include <linux/regulator/consumer.h>
 #include <linux/leds-qpnp-wled.h>
 #include <linux/clk.h>
+#ifdef CONFIG_LCD_NOTIF
 #include <linux/lcd_notify.h>
+#endif
 #include <linux/uaccess.h>
 #include <linux/msm-bus.h>
 #include <linux/pm_qos.h>
@@ -2526,7 +2528,9 @@ static int mdss_dsi_event_handler(struct mdss_panel_data *pdata,
 							pdata);
 		break;
 	case MDSS_EVENT_UNBLANK:
+#ifdef CONFIG_LCD_NOTIF
 		lcd_notifier_call_chain(LCD_EVENT_ON_START, NULL);
+#endif
 		if (ctrl_pdata->on_cmds.link_state == DSI_LP_MODE)
 			rc = mdss_dsi_unblank(pdata);
 		break;
@@ -2538,13 +2542,14 @@ static int mdss_dsi_event_handler(struct mdss_panel_data *pdata,
 		if (ctrl_pdata->on_cmds.link_state == DSI_HS_MODE)
 			rc = mdss_dsi_unblank(pdata);
 		pdata->panel_info.esd_rdy = true;
+#ifdef CONFIG_LCD_NOTIF
 		lcd_notifier_call_chain(LCD_EVENT_ON_END, NULL);
-#ifdef CONFIG_STATE_NOTIFIER
-		state_resume();
 #endif
 		break;
 	case MDSS_EVENT_BLANK:
+#ifdef CONFIG_LCD_NOTIF
 		lcd_notifier_call_chain(LCD_EVENT_OFF_START, NULL);
+#endif
 		power_state = (int) (unsigned long) arg;
 		if (ctrl_pdata->off_cmds.link_state == DSI_HS_MODE)
 			rc = mdss_dsi_blank(pdata, power_state);
@@ -2555,9 +2560,8 @@ static int mdss_dsi_event_handler(struct mdss_panel_data *pdata,
 		if (ctrl_pdata->off_cmds.link_state == DSI_LP_MODE)
 			rc = mdss_dsi_blank(pdata, power_state);
 		rc = mdss_dsi_off(pdata, power_state);
+#ifdef CONFIG_LCD_NOTIF
 		lcd_notifier_call_chain(LCD_EVENT_OFF_END, NULL);
-#ifdef CONFIG_STATE_NOTIFIER
-		state_suspend();
 #endif
 		break;
 	case MDSS_EVENT_CONT_SPLASH_FINISH:
